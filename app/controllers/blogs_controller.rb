f8cf192d286d23e08f3fbf01147c7f7140a68985
george kettle <georgekettle.gk@gmail.com>
2023-07-16 22:37:28 +0200

    IMage upload working

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -31,6 +31,6 @@ class BlogsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def blog_params
-        params.require(:blog).permit(:title, :content)
+        params.require(:blog).permit(:title, :content, :cover_image)
     end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,7 @@
 class Blog < ApplicationRecord
+    has_one_attached :cover_image
+
+    validates :title, presence: true
+    validates :content, presence: true, length: { minimum: 10 }
+    validates :cover_image, presence: true
 end
