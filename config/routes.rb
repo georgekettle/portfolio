1c14c43c5a423d75255d09428c4948f5c4651ca9
george kettle <georgekettle.gk@gmail.com>
2023-07-16 13:46:53 +0200

    Updated routes so that portfolio contains portfolio information

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   get 'download_resume', to: 'pages#download_resume', as: :download_resume
   get 'testimonials', to: 'pages#testimonials', as: :testimonials
+  get 'portfolio', to: 'pages#portfolio', as: :portfolio
   root "pages#home"
 end
