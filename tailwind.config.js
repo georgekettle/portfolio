5d45c36e99bbeee30da3e8a7b669fe3f85d8b5b4
george kettle <georgekettle.gk@gmail.com>
2023-07-16 20:59:56 +0200

    Basic design for home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
@@ -7,53 +9,18 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      sans: ['Apercu', 'sans-serif'],
-      header: ['GT Pressura', 'sans-serif']
+      sans: ['Satoshi', 'sans-serif'],
+      header: ['Satoshi', 'sans-serif']
     },
     extend: {
       colors: {
         bg: {
           light: '#fffbf9'
         },
-        primary: {
-          '50': '#f5f2fc',
-          '100': '#e6e2f8',
-          '200': '#d6cef3',
-          '300': '#b8aae9',
-          '400': '#957cda',
-          '500': '#7954c9',
-          '600': '#6a43a8',
-          '700': '#574474',
-          '800': '#403252',
-          '900': '#2a2334',
-        },
-        secondary: {
-          '50': '#fff8eb',
-          '100': '#fff3d6',
-          '200': '#ffe2a8',
-          '300': '#ffc766',
-          '400': '#fea939',
-          '500': '#f58f19',
-          '600': '#e17414',
-          '700': '#b45918',
-          '800': '#8b4923',
-          '900': '#6b3f24',
-        },       
+        tertiary: colors.stone,
+        primary: colors.sky,
+        secondary: colors.purple,    
       },
-      animation: {
-        'marqueeone': 'marqueeone 50s linear infinite',
-        'marqueetwo': 'marqueetwo 50s linear infinite'
-      },
-      keyframes: {
-        marqueeone: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' }
-        },
-        marqueetwo: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(0%)' }
-        }
-      }
     },
   },
 }
