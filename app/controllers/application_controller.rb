db127df536f3ff670479a527afa78095bbe11fcc
george kettle <georgekettle.gk@gmail.com>
2023-07-17 22:33:13 +0200

    Styled animation of blog card

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,17 @@ class ApplicationController < ActionController::Base
     private
 
     def set_default_meta_tags
-        # helpers = ActionController::Base.helpers
+        helpers = ActionController::Base.helpers
         set_meta_tags(
-            title: "George Kettleeeee",
             description: "Soloproneur, Rails Developer, and lover of Cycling & Skiing.",
             keywords: "George Kettle, Rails, Ruby, Small business, Soloproneur",
-            # url: request.original_url,
-            # icon: helpers.image_url("favicon.png"),
-            # image: helpers.image_url("default_og_image.jpg"),
-            # twitter: {
-            #     card: "summary",
-            #     site: "@george_kettle",
-            # }
+            url: request.original_url,
+            icon: helpers.image_url("favicon.png"),
+            image: helpers.image_url("default_og_image.jpg"),
+            twitter: {
+                card: "summary",
+                site: "@george_kettle",
+            }
         )
     end
 end
