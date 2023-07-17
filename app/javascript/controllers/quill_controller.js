09702ebd63400c1cb2b641064067cf7e60bb6724
george kettle <georgekettle.gk@gmail.com>
2023-07-17 19:33:04 +0200

    Stimulus controller created for quill

--- app/javascript/controllers/quill_controller.js
+++ app/javascript/controllers/quill_controller.js
@@ -1,7 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
+import Quill from 'quill'
 
-// Connects to data-controller="quill"
 export default class extends Controller {
-  connect() {
+  initialize() {
+    const element = this.element;
+    const editor = new Quill(element, {
+      theme: 'snow' // You can customize the theme here
+    });
+
+    editor.on('text-change', () => {
+      element.value = editor.root.innerHTML;
+    });
   }
 }
