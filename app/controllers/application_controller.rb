7e5e0cd7919f67fc46a75b3319d3537b00a9cb5e
george kettle <georgekettle.gk@gmail.com>
2023-07-17 21:12:45 +0200

    Added default meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    before_action :set_default_meta_tags
+
+    private
+
+    def set_default_meta_tags
+        set_meta_tags(
+            title: "George Kettle",
+            description: "Soloproneur, Rails Developer, and lover of Cycling & Skiing.",
+            keywords: "George Kettle, Rails, Ruby, Cycling, Skiing, Soloproneur",
+          )
+    end
 end
