d1b79548c0bfa72d966f3ade12f6ea0127a427fb
george kettle <georgekettle.gk@gmail.com>
2023-07-17 20:25:01 +0200

    Removed content field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_175120) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_17_182428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,7 +54,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_17_175120) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
-    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
